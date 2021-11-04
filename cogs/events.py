3c81d6905c661bc053a434cc259cf8fbb86aac57
gritor111 <lironfrid2007@gmail.com>
2021-11-04 17:49:46 +0200

    fixed the last fix of the last fix of last fix

--- cogs/events.py
+++ cogs/events.py
@@ -24,7 +24,7 @@ class Events(commands.Cog):
                     last_pray_timestamp = (await self.bot.hdb.get_last_pray_user(prayer_username))[0]['timestamp']
                     await self.bot.hdb.add_pray(prayer_username)
                     print(last_pray_timestamp)
-                    print((last_pray_timestamp - datetime.datetime.now(datetime.timezone.utc)) >= datetime.time(minute=5), (last_pray_timestamp - datetime.datetime.utcnow()), datetime.time(minute=5))
+                    print((last_pray_timestamp - datetime.datetime.now(datetime.timezone.utc)) >= datetime.timedelta(minute=5), (last_pray_timestamp - datetime.datetime.utcnow()), datetime.time(minute=5))
                     #if last_pray_timestamp:  # checking if user prayed before
                         #if (last_pray_timestamp - datetime.datetime.utcnow()) >= datetime.time(minute=5):
 
