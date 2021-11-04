eb54378241f48edd19b4d401e1b8bf110416f6fe
gritor111 <lironfrid2007@gmail.com>
2021-11-04 23:13:45 +0200

    add first pray support

--- cogs/events.py
+++ cogs/events.py
@@ -29,6 +29,7 @@ class Events(commands.Cog):
                         if (datetime.datetime.now(datetime.timezone.utc) - last_pray[0]["timestamp"])\
                                 >= datetime.timedelta(minutes=5):
                             await self.bot.hdb.add_pray(prayer_username)
+
                     else:  # first ever pray
                         await self.bot.hdb.add_pray(prayer_username)
 
