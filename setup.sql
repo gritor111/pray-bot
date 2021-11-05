ab197016d19f85125469fdc335eaca85b399228c
gritor111 <lironfrid2007@gmail.com>
2021-11-05 22:21:47 +0200

    update sql

--- setup.sql
+++ setup.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS users (
   username VARCHAR NOT NULL,
-  pray_count INT DEFAULT 0
+  pray_count INT DEFAULT 0,
+  user_id BIGINT
 )
 
 CREATE TABLE IF NOT EXISTS pray_logs (
   username VARCHAR NOT NULL,
-  timestamp TIMESTAMP WITH TIME ZONE
+  timestamp TIMESTAMP WITH TIME ZONE,
+  user_id BIGINT
 )
