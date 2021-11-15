e9d81ee91aafb75b0450a8ca0283e0fc1a682591
gritor111 <lironfrid2007@gmail.com>
2021-11-15 23:42:56 +0200

    try something

--- cogs/personal.py
+++ cogs/personal.py
@@ -48,8 +48,8 @@ class Personal(commands.Cog):
         embed = discord.Embed(color=discord.Color.orange())
         embed.set_author(name=f"{ctx.author.name}'s profile", icon_url=str(ctx.author.avatar_url))
         embed.set_thumbnail(url=str(ctx.author.avatar_url))
-        embed.add_field(name="Level", value="`" + str(user_lvl) + "`")
-        embed.add_field(name="Experience", value=f"`[{user_xp}/{required_xp}]`\n{progress}")
+        embed.add_field(name="Level", value="`" + str(user_lvl) + "`", inline=False)
+        embed.add_field(name="Experience", value=f"`[{user_xp}/{required_xp}]`\n{progress}", inline=False)
 
         await ctx.channel.send(embed=embed)
 
