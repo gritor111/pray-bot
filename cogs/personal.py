7fc69e106791d18ec8e4356194ecc131144adc4a
gritor111 <lironfrid2007@gmail.com>
2021-11-15 21:10:28 +0200

    fancify the fancy stuff

--- cogs/personal.py
+++ cogs/personal.py
@@ -14,13 +14,13 @@ class Personal(commands.Cog):
         user = await self.bot.hdb.get_user(ctx.author.name)
         user_lvl = user[0]["level"]
         user_xp = user[0]["current_xp"]
-        required_xp = 5000 + math.pow(user_lvl * self.bot.util.XP_MULTI, 2)
+        required_xp = int(5000 + math.pow(user_lvl * self.bot.util.XP_MULTI, 2))
         progress = ("■" * int((user_xp / required_xp) * 10)).ljust(10, "□")
 
-        embed = discord.Embed()
-        embed.set_author(name=f"**{ctx.author.name}'s profile**", icon_url=str(ctx.author.avatar_url))
-        embed.add_field(name="Level", value=user_lvl)
-        embed.add_field(name="Experience", value=f"[{user_xp}/{required_xp}]\n{progress}")
+        embed = discord.Embed(color=discord.Color.orange())
+        embed.set_author(name=f"{ctx.author.name}'s profile", icon_url=str(ctx.author.avatar_url))
+        embed.add_field(name="**Level**", value="`" + user_lvl + "`")
+        embed.add_field(name="**Experience**", value=f"`[{user_xp}/{required_xp}]`\n{progress}")
 
         await ctx.channel.send(embed=embed)
 
