e2191b31e3d7000d857f0214672038c992aa57f4
gritor111 <lironfrid2007@gmail.com>
2021-11-15 22:33:51 +0200

    revert stuff

--- cogs/personal.py
+++ cogs/personal.py
@@ -12,7 +12,7 @@ class Personal(commands.Cog):
     async def profile(self, ctx):
 
         # what the fuck is this
-        progress_bar_full_1 = self.bot.get_emoji(909895429540421672)
+        progress_bar_full_1 = "<a:Bar1Full:909895429540421672>"
         progress_bar_full_2 = "<a:Bar2Full:909895429322313850>"
         progress_bar_empty_1 = "<:Bar1Empty:909897948698128395>"
         progress_bar_empty_2 = "<:Bar2Empty:909895429217460265>"
@@ -31,7 +31,7 @@ class Personal(commands.Cog):
         embed.add_field(name="Experience", value=f"`[{user_xp}/{required_xp}]`\n{progress}")
 
         await ctx.channel.send(embed=embed)
-        await ctx.channel.send(progress_bar_full_1)
+        await ctx.channel.send(progress_bar_full_1 + ' ' + progress_bar_full_2 + ' ' + progress_bar_empty_1 + ' ' + progress_bar_empty_2 + ' ' + progress_bar_empty_3)
 
 
 def setup(bot):
