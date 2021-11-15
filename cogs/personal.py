989e045d7affbee5bc5ab44a537344738ed2a5e2
gritor111 <lironfrid2007@gmail.com>
2021-11-15 22:28:08 +0200

    maybe this will work v2

--- cogs/personal.py
+++ cogs/personal.py
@@ -12,7 +12,7 @@ class Personal(commands.Cog):
     async def profile(self, ctx):
 
         # what the fuck is this
-        progress_bar_full_1 = "<a:Bar1Full:909895429540421672>"
+        progress_bar_full_1 = self.bot.get_emoji(909895429540421672)
         progress_bar_full_2 = "<a:Bar2Full:909895429322313850>"
         progress_bar_empty_1 = "<:Bar1Empty:909897948698128395>"
         progress_bar_empty_2 = "<:Bar2Empty:909895429217460265>"
