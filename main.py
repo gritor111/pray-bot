eae082655d01bc3e111f380e0726f34e82f1117d
gritor111 <lironfrid2007@gmail.com>
2021-11-19 13:29:20 +0200

    add intents

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 import asyncpg
 from discord.ext import commands
-import datetime
+import discord
 from util import xp_funcs, help
 
 
 async def setup_db():
     bot.db = await asyncpg.create_pool(dsn=os.getenv("DATABASE_URL"), max_size=5, min_size=1)
 
+intents = discord.Intents.default()
+intents.members = True
 
 prefix = os.getenv('PREFIX')
 bot = commands.Bot(command_prefix=[prefix, prefix.upper()], owner_id=656373241144934420, case_insensitive=True,
