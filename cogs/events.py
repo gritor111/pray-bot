7863766e49f5716f94a6377e88b7a0ce673bda39
gritor111 <lironfrid2007@gmail.com>
2021-11-20 22:37:27 +0200

    debug

--- cogs/events.py
+++ cogs/events.py
@@ -113,6 +113,7 @@ class Events(commands.Cog):
     async def update_active(self):
         guild = await self.bot.fetch_guild(888467716732747827)
         active_role = guild.get_role(911639659430432838)
+        print(guild)
         members = guild.members
         print(members)
         for member in members:
