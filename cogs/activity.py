475da090553e09a5d81c0c840e824b23c64f5a1d
gritor111 <lironfrid2007@gmail.com>
2021-11-20 17:11:09 +0200

    add active command

--- cogs/activity.py
+++ cogs/activity.py
@@ -21,16 +21,20 @@ class Activity(commands.Cog):
 
         await ctx.channel.send(embed=embed)
 
-    @commands.command()
+    @commands.command(name="active", aliases=["weekly", "week"])
     async def weekly(self, ctx, *, member: discord.Member = None):
         user = ctx.author
 
         if member:
             user = member
 
-        daily_pray_count = len(await self.bot.hdb.get_count_by_time(user, '1 week'))
+        weekly_pray_count = len(await self.bot.hdb.get_count_by_time(user, '1 week'))
 
-        embed = discord.Embed(title=f"{user.name}'s weekly pray count: {daily_pray_count}", color=discord.Color.green())
+        embed = discord.Embed(color=discord.Color.green())
+        embed.set_author(name=member.name, icon_url=str(user.avatar_url))
+
+        if weekly_pray_count >= 200:
+            embed.description = f"**{user.name}** has prayed to louis **{weekly_pray_count} times in the last week"
 
         await ctx.channel.send(embed=embed)
 
